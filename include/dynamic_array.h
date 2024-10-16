90023dface9bb445efd25abf5e4412811525fc2e
MinSeok Oh <minseok.dev@gmail.com>
2024-10-17 00:14:59 +0900

    feat: 기본 동적 배열 함수 정의

--- include/dynamic_array.h
+++ include/dynamic_array.h
@@ -4,10 +4,22 @@
 #include <stdlib.h>
 
 typedef struct {
-    void* data;
-    size_t element_size;
-    size_t size;
-    size_t capacity;
+  void *data;
+  size_t element_size;
+  size_t size;
+  size_t capacity;
 } DynamicArray;
+
+typedef enum { DYNAMIC_ARRAY_SUCCESS = 0 } DynamicArrayError;
+
+void dynamic_array_init(DynamicArray *dynamicArray, size_t element_size);
+void dynamic_array_free(DynamicArray *dynamicArray);
+
+void *dynamic_array_peek(DynamicArray *dynamicArray, size_t index);
+DynamicArrayError dynamic_array_insert(DynamicArray *dynamicArray, size_t index,
+                                       void *element);
+DynamicArrayError dynamic_array_delete(DynamicArray *dynamicArray,
+                                       size_t index);
+DynamicArrayError dynamic_array_realloc(DynamicArray *dynamicArray);
 
 #endif
