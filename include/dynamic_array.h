fc364cf23103e3dcb40e50001a9139260ebd3081
MinSeok Oh <minseok.dev@gmail.com>
2024-10-16 22:54:36 +0900

    feat: 동적 배열에 요소 크기, size, capacity 추가

--- include/dynamic_array.h
+++ include/dynamic_array.h
@@ -1,8 +1,13 @@
 #ifndef DYNAMIC_ARRAY_H
 #define DYNAMIC_ARRAY_H
 
+#include <stdlib.h>
+
 typedef struct {
     void* data;
+    size_t element_size;
+    size_t size;
+    size_t capacity;
 } DynamicArray;
 
 #endif
